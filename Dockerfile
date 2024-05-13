69a97f7a5e8e384cd32b2491ca7de2a7145497e1
Mosè Giordano <mose@gnu.org>
2024-05-13 11:58:49 +0100

    First dummy version of notebook and GHA workflow to publish docker image

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# syntax=docker/dockerfile:1
+
+FROM --platform=linux/amd64 julia:1.10.3
+
+# Set `JULIA_CPU_TARGET` to same value as default Julia builds, to ensure we
+# don't specialise pkgimages to the current host CPU:
+# https://github.com/JuliaCI/julia-buildkite/blob/ea50eb719242bc3e844227a2ebf54a49d308bafc/utilities/build_envs.sh#L25-L32
+ENV JULIA_CPU_TARGET="generic;sandybridge,-xsaveopt,clone_all;haswell,-rdrnd,base(1);x86-64-v4,-rdrnd,base(1)"
+
+# Instantiate Julia project
+RUN mkdir -p /root/.julia/environments/v1.10
+COPY Project.toml  /root/.julia/environments/v1.10/Project.toml
+COPY Manifest.toml /root/.julia/environments/v1.10/Manifest.toml
+RUN julia -e 'using Pkg; Pkg.instantiate()'
+
+# Copy notebook
+COPY notebook.jl /root/notebook.jl
+
+# Precompile notebook environment
+RUN julia -e '
+using Pkg
+using Pluto
+Pluto.activate_notebook_environment("notebook.jl")
+Pkg.instantiate()
+'
---
+++ notebook.jl
@@ -0,0 +1,44 @@
+### A Pluto.jl notebook ###
+# v0.19.42
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ ff224232-2f13-4ccb-abc2-12bee286e4e9
+using Example: hello
+
+# ╔═╡ f8d42002-1113-11ef-2f39-9f940d22fa8b
+hello("world")
+
+# ╔═╡ b373e1c8-ced7-4d8a-b84e-8e75fdfc2f5b
+1 + 2.0
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+Example = "7876af07-990d-54b4-ab0e-23690620f79a"
+
+[compat]
+Example = "~0.5.3"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.10.3"
+manifest_format = "2.0"
+project_hash = "47cae4debbf51b7dd7886b2d682969c7c9a8e882"
+
+[[deps.Example]]
+git-tree-sha1 = "46e44e869b4d90b96bd8ed1fdcf32244fddfb6cc"
+uuid = "7876af07-990d-54b4-ab0e-23690620f79a"
+version = "0.5.3"
+"""
+
+# ╔═╡ Cell order:
+# ╠═ff224232-2f13-4ccb-abc2-12bee286e4e9
+# ╠═f8d42002-1113-11ef-2f39-9f940d22fa8b
+# ╠═b373e1c8-ced7-4d8a-b84e-8e75fdfc2f5b
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
