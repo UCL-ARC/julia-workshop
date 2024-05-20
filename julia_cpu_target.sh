8335ddcd278a4270005c6dff6e6848d5662dbce5
Mosè Giordano <mose@gnu.org>
2024-05-20 16:33:20 +0100

    Remove `x86-64-v4` from `JULIA_CPU_TARGET`

--- julia_cpu_target.sh
+++ julia_cpu_target.sh
@@ -5,7 +5,7 @@
 # https://github.com/JuliaCI/julia-buildkite/blob/ea50eb719242bc3e844227a2ebf54a49d308bafc/utilities/build_envs.sh#L25-L32
 # https://github.com/JuliaCI/julia-buildkite/blob/ea50eb719242bc3e844227a2ebf54a49d308bafc/utilities/build_envs.sh#L58-L69
 if [ "$(arch)" = "x86_64" ]; then
-    export JULIA_CPU_TARGET='generic;sandybridge,-xsaveopt,clone_all;haswell,-rdrnd,base(1);x86-64-v4,-rdrnd,base(1)'
+    export JULIA_CPU_TARGET='generic;sandybridge,-xsaveopt,clone_all;haswell,-rdrnd,base(1)'
 elif [ "$(arch)" = "aarch64" ]; then
     export JULIA_CPU_TARGET='generic;carmel,clone_all;apple-m1,base(1)'
 fi
